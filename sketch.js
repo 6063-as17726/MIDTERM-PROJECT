8ee7abf1b11e9e74976edae415782f5ba660ad42
Ayushi Shah <as17726@nyu.edu>
2023-10-12 17:58:10 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,20 @@
 
-angleMode(DEGREES); 
-rectMode(CENTER); 
-ellipseMode(RADIUS); 
+//angleMode(DEGREES); 
+//rectMode(CENTER); 
+//ellipseMode(RADIUS); 
 
 class Shapes {
   constructor() { 
     this.xPos = random(0, width); 
     this.yPos = random(0, height); 
-    this.x = 10; 
-    this.y = 10;  
+    this.x = 50; 
+    this.y = 50;  
+    this.color = 0; 
   }
   drawShapes() {
-    fill(0); 
-    ellipse(this.xPos, this.yPos, x, y); 
+    fill(this.color);
+    ellipse(this.xPos, this.yPos, this.x, this.y);
+  }
     //for (i = 0; i<3; i++) {
      // let a = rect(this.xPos, this.yPos, this.x, this.y); 
       //let b = ellipse(this,xPos, this.yPos, this.x, this.y); 
@@ -20,15 +22,14 @@ class Shapes {
       //random(a, b, c);
     //}
   //drawMainShape() 
-  }
 }
 
-let numShapes = 50; 
 let shapeArray = []; 
+let numShapes = 50; 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (let d = 0; d< numShapes; d++) {
+  for (let d = 0; d < numShapes; d++) {
     let aShape = new Shapes(); 
     shapeArray.push(aShape); 
   }
@@ -38,12 +39,14 @@ function createLayout() {
   strokeWeight(2); 
   rectMode(CENTER); 
   background (116,196,214);
+  push(); 
   translate(width/2, height/2); 
   rotate(-0.1); 
   fill(255); 
   rect (0,0, 350, 450 ); 
   fill(116,196,214); 
   rect (0, 0, 300, 350 ); 
+  pop();
 }
 
 function draw() {
