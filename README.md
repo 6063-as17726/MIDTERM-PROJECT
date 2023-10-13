510cd40803a6076bf18af9453b0f7117636633d9
Ayushi Shah <as17726@nyu.edu>
2023-10-13 18:41:35 -0400

    Trying a new concept

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# p5.js Template
 
 I chose the book - "Where's Waldo" which is an interactive book, where the goal is to find the character Waldo. 
 I want to include the concept of chaos in my program, inspired by the chaos on the book cover. 
 I want to make an interactive piece where there are random, abstract forms all over the canvas, and the goal is to find a specific type of form, that would look like Waldo. Or create a canvas where the user can create their own abstract forms all over. The forms could also move with passing time. 
 I would want to experiment with mouse click functions, and also functions (My idea is to create different functions with different abstract forms and call them randomely). Maybe I can also use arrays to store the different shape data points?
 I guess I could relate this idea to the concept of interactivity, which we read about in "The Art of Interactive Design" by Chris Crawford, where I would want to address form and function. 
+
+Concept revision - 
+After getting started with the code, I realized that it is extremely difficult for me to create the "game-like" functioning of the code. 
+So I decided to rethink the click functions to be interactive in populating different characters on the screen using classes and arrays and click functions. 
+A fun second step could also be to add other interactive functions to change colors, shapes, etc. 
+
 
 ![DATZM4.jpg](https://imgpile.com/images/DATZM4.jpg)
--- sketch.js
+++ sketch.js
@@ -1,89 +1,42 @@
+let minDiameter;
+let maxDiameter;
 
-//angleMode(DEGREES); 
-//rectMode(CENTER); 
-//ellipseMode(RADIUS); 
-
-class Shapes {
-  constructor() { 
-    this.xPos = random(0, width); 
-    this.yPos = random(0, height); 
-    this.x = 50; 
-    this.y = 50;  
-    this.color = 0; 
-  }
-  drawShapes() {
-    fill(this.color);
-    ellipse(this.xPos, this.yPos, this.x, this.y);
-  }
-
-  checkOverlap(otherShape) {
-    // check if comparing to itself
-    if (this == otherShape) {
-      return false;
-    }
-    // use distance between 2 circles and radius to check overlap
-    let cDist = dist(this.x, this.y, otherShape.x, otherShape.y);
-    return cDist < this.x + otherCircle.x;
-  }
-  
-  checkAllOverlaps() {
-    let overlap = false;
-    for (let ci = 0; ci < shapeArray.length; ci++) {
-      let aShape = shapeArray[ci];
-      if (this.checkOverlap(aShape)) {
-        overlap = true;
-      }
-      if (overlap) {
-        this.xPos = random(0, width); 
-        this.yPos = random(0, height); 
-      } 
-    }
-  }
-  
-    //for (i = 0; i<3; i++) {
-     // let a = rect(this.xPos, this.yPos, this.x, this.y); 
-      //let b = ellipse(this,xPos, this.yPos, this.x, this.y); 
-      //let c = triangle(this.xPos, this.yPos, this.xPos - this.x, this.yPos, this.xPos, this.yPos - this.y); 
-      //random(a, b, c);
-    //}
-  //drawMainShape() 
-}
-
-let shapeArray = []; 
-let numShapes = 100; 
+let myShapes = [];
+let maxNumShapes = 200;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (let d = 0; d < numShapes; d++) {
-    let aShape = new Shapes(); 
-    shapeArray.push(aShape); 
-  }
+  maxDiameter = width / 10;
+  minDiameter = maxDiameter / 3;
 }
 
-function createLayout() {
-  strokeWeight(2); 
-  rectMode(CENTER); 
-  background (116,196,214);
-  push(); 
-  translate(width/2, height/2); 
-  rotate(-0.1); 
-  fill(255); 
-  rect (0,0, 350, 450 ); 
-  fill(116,196,214); 
-  rect (0, 0, 300, 350 ); 
-  pop();
+function drawShape(aShape) {
+  ellipse(aShape.x, aShape.y, aShape.d, aShape.d);
 }
 
 function draw() {
-  createLayout(); 
-  for (let d = 0; d < shapeArray.length; d++) {
-    let aShape = shapeArray[d];
-
-    // draw
-    aShape.drawShapes();
-    aShape.checkAllOverlaps(); 
-  }
-
+  background(239, 252, 255);
+  myShapes.forEach(drawShape);
 }
 
+function addShape() {
+  let newShape = {
+    x: random(width),
+    y: random(height),
+    d: random(minDiameter, maxDiameter),
+  };
+  myShapes.push(newShape);
+}
 
+function mouseClicked() {
+  // if more than maxCircles, then add the top box 
+  if (myShapes.length >= maxNumShapes) {
+    myShapes = [];
+  } else {
+    let shapesToAdd = random(8, 16);
+    for (let c = 0; c < shapesToAdd; c++) {
+      addShape();
+      fill (random(0,255), random(0,255), random (0,255)); 
+    }
+  }
+}
