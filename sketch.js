976d6f86f581d5af40c0f37f6e966dd821786f33
Ayushi Shah <as17726@nyu.edu>
2023-10-15 16:49:33 -0400

    4 characters added

--- sketch.js
+++ sketch.js
@@ -6,36 +6,42 @@
 
 function character1() {
   // Body
-  fill(255, 150, 100);
-  stroke(0);
-  strokeWeight(1);
-  beginShape();
-  vertex(75, 150);
-  vertex(100, 100);
-  vertex(125, 150);
-  endShape(CLOSE);
+ fill(0);
+ stroke(0);
+ strokeWeight(2);
+ rect(90, 100, 20, 50);
 
-  // Head
-  fill(255, 200, 150);
-  noStroke();
-  ellipse(100, 75, 60, 60);
+ //face 
+ strokeWeight(0); 
+ fill("LightSalmon"); 
+ ellipse(100, 85, 30,30); 
 
-  // Eyes
-  fill(255);
-  ellipse(85, 65, 15, 15);
-  ellipse(115, 65, 15, 15);
+ // Glasses
+ fill(0);
+ noStroke();
+ ellipse(95,80,10,10);
+ ellipse(105,80,10,10);
 
-  // Arms
-  fill(255, 150, 100);
-  noStroke();
-  rect(50, 100, 100, 20);
+ // Striped Shirt
+ fill(255, 255, 0);
+ noStroke();
+ rect(90, 100, 20, 10);
+ rect(90, 115, 20, 10);
+ rect(90, 130, 20, 10);
 
-  // Legs
-  fill(255, 150, 100);
-  rect(85, 150, 10, 100);
-  rect (105,150,10,100);
+ // Hat
+ fill(0);
+ noStroke();
+ triangle(80, 70, 120, 70, 100, 50);
+ fill(255,255,0); 
+ ellipse(100,45,10,10);
+
+
+ // Pants
+ fill(0);
+ noStroke();
+ rect(90, 150, 20, 20);
 }
-
 function waldo() {
  // Body
  fill(255, 0, 0);
@@ -55,7 +61,7 @@ function waldo() {
  ellipse(105,80,10,10);
 
  // Striped Shirt
- fill(255, 255, 0);
+ fill(255, 255, 255);
  noStroke();
  rect(90, 100, 20, 10);
  rect(90, 115, 20, 10);
@@ -76,6 +82,103 @@ function waldo() {
  noStroke();
  rect(90, 150, 20, 20);
 }
+function character2() {
+//stick
+ strokeWeight(1); 
+ stroke(0); 
+ fill(255); 
+ rect(115,30,5,140); 
+
+ //stripes 
+ strokeWeight(0); 
+ fill(255,0,0); 
+ rect(115,30,5,15); 
+ rect(115,90,5,15); 
+ rect(115,150,5,15); 
+ fill(0,0,255); 
+ rect(115,60,5,15); 
+ rect(115,120,5,15); 
+
+
+ // Body
+ fill(255, 0, 0);
+ stroke(0);
+ strokeWeight(1);
+ beginShape();
+ vertex (90, 100); 
+ vertex(80,170); 
+ vertex(120,170);
+ vertex(110,100);
+ endShape();  
+
+ //beard 
+ strokeWeight(1); 
+ stroke(0); 
+ fill(255); 
+ triangle(90,90,110,90, 100,140);
+ 
+ //face 
+ strokeWeight(0); 
+ fill("LightSalmon"); 
+ ellipse(100, 85, 30,30); 
+
+ // Hat
+ fill(0,0,255);
+ noStroke();
+ triangle(80, 75, 120, 75, 100, 40);
+
+}
+function character3() {
+  // Body
+ fill(255, 0, 0);
+ stroke(0);
+ strokeWeight(2);
+ rect(90, 100, 20, 25);
+ rect(90, 145, 20, 25);
+
+  //hair 
+  strokeWeight(0); 
+  fill(108,80,26);
+  triangle(80, 100, 120, 100, 100, 50);
+
+ //face 
+ strokeWeight(0); 
+ fill("LightSalmon"); 
+ ellipse(100, 85, 30,30); 
+
+ // Glasses
+ fill(255);
+ noStroke();
+ ellipse(95,80,10,10);
+ ellipse(105,80,10,10);
+
+ // Striped Shirt
+ fill(255, 255, 255);
+ noStroke();
+ rect(90, 100, 20, 10);
+ rect(90, 115, 20, 10);
+ rect(90, 150, 20, 10);
+
+ // Hat
+ fill(255, 0, 0);
+ noStroke();
+ triangle(80, 70, 120, 70, 100, 50);
+ strokeWeight(1); 
+ stroke(255, 0, 0); 
+ fill(255); 
+ ellipse(100,45,10,10);
+
+
+ //Skirt
+ fill(0, 0, 255);
+ noStroke();
+ rect(87, 125, 25, 20);
+}
+
+
+
+
+
 function setup() {
    createCanvas(windowWidth, windowHeight);
    background(240); 
@@ -90,6 +193,18 @@ function setup() {
  function draw() {
   //character1(); 
   waldo(); 
+  push(); 
+  translate (100,0); 
+  character1();
+  pop(); 
+  push(); 
+  translate (200,0); 
+  character2();
+  pop();  
+  push(); 
+  translate (300,0); 
+  character3();
+  pop(); 
 //   background(239, 252, 255);
 //   myShapes.forEach(drawShape);
  }
