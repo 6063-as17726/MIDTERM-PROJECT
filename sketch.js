63509b3000e99cbb9df855355bbf72fa31321ef5
Ayushi Shah <as17726@nyu.edu>
2023-10-19 16:42:12 -0400

    Fixed the loops

--- sketch.js
+++ sketch.js
@@ -1,96 +1,98 @@
-//How to run the mouseclick function to identify Waldo 
 //How to avoid/decrease overlap of the characters 
 
 function character0(X,Y) {
   push();
   translate(X,Y);
-  pop(); 
-
+  
   // Body
  fill(0);
  stroke(0);
  strokeWeight(2);
- rect(90, 100, 20, 50);
+ rect(0, 10, 20, 50);
 
  //face 
  strokeWeight(0); 
  fill("LightSalmon"); 
- ellipse(100, 85, 30,30); 
+ ellipse(10, 15, 30,30); 
 
  // Glasses
  fill(0);
  noStroke();
- ellipse(95,80,10,10);
- ellipse(105,80,10,10);
+ ellipse(5,10,10,10);
+ ellipse(15,10,10,10);
 
  // Striped Shirt
  fill(255, 255, 0);
  noStroke();
- rect(90, 100, 20, 10);
- rect(90, 115, 20, 10);
- rect(90, 130, 20, 10);
+ rect(0, 25, 20, 10);
+ rect(0, 40, 20, 10);
+ rect(0, 55, 20, 10);
 
  // Hat
  fill(0);
  noStroke();
- triangle(80, 70, 120, 70, 100, 50);
+ triangle(-10,5, 30,5, 10, -10);
  fill(255,255,0); 
- ellipse(100,45,10,10);
+ ellipse(10,-10,10,10);
 
 
  // Pants
  fill(0);
  noStroke();
- rect(90, 150, 20, 20);
+ rect(0, 60, 20, 20);
+
+ pop(); 
 }
 function waldo(X,Y) {
   push();
   translate(X,Y);
-  pop(); 
 
  // Body
  fill(255, 0, 0);
  stroke(0);
  strokeWeight(2);
- rect(90, 100, 20, 50);
+ rect(0, 10, 20, 50);
 
  //face 
  strokeWeight(0); 
  fill("LightSalmon"); 
- ellipse(100, 85, 30,30); 
+ ellipse(10, 15, 30,30); 
 
  // Glasses
  fill(255);
  noStroke();
- ellipse(95,80,10,10);
- ellipse(105,80,10,10);
+ ellipse(5,10,10,10);
+ ellipse(15,10,10,10);
 
  // Striped Shirt
  fill(255, 255, 255);
  noStroke();
- rect(90, 100, 20, 10);
- rect(90, 115, 20, 10);
- rect(90, 130, 20, 10);
+ rect(0, 25, 20, 10);
+ rect(0, 40, 20, 10);
+ rect(0, 55, 20, 10);
 
  // Hat
  fill(255, 0, 0);
  noStroke();
- triangle(80, 70, 120, 70, 100, 50);
+ triangle(-10,5, 30,5, 10, -10);
  strokeWeight(1); 
  stroke(255, 0, 0); 
  fill(255); 
- ellipse(100,45,10,10);
+ ellipse(10,-10,10,10);
 
 
  // Pants
  fill(0, 0, 255);
  noStroke();
  rect(90, 150, 20, 20);
+
+ pop(); 
 }
+
+//need to update
 function character1(X,Y) {
   push();
   translate(X,Y);
-  pop(); 
 
 //stick
  strokeWeight(1); 
@@ -136,12 +138,14 @@ function character1(X,Y) {
  noStroke();
  triangle(80, 75, 120, 75, 100, 40);
 
+ pop(); 
 }
+
+//need to update
 function character2(X,Y) {
   push();
   translate(X,Y);
-  pop(); 
-
+  
   // Body
  fill(255, 0, 0);
  stroke(0);
@@ -186,7 +190,10 @@ function character2(X,Y) {
  fill(0, 0, 255);
  noStroke();
  rect(87, 125, 25, 20);
+
+ pop();
 }
+
 function banner() {
   rectMode(CENTER); 
   background (116,196,214);
@@ -203,23 +210,24 @@ function banner() {
 function setup() {
   createCanvas(800, windowHeight);
    background(240); 
-   //noloop(); 
+   noLoop(); 
  }
 
- function draw() {
-  let F = [character0(),character1(), character2()]; 
-  let rowheight = windowHeight/7; 
+function draw() {
+  let F = [character0,character1, character2]; 
+  let rowheight = windowHeight/7;  
   let xpos = random(0, width); 
   let ypos = random(0, rowheight);  
-  for (let y = 0; y<height; y+=rowheight) {
-    for (let i = 0; i<20; i++) {
-      let myFun = random(F);
-      dy = random(y,y+rowheight/2); 
-      x = random (0, width); 
-      myFun(x,dy); 
-      ypos = random(dy, ypos);  
-    }
+
+for (let y = 0; y<height; y+=rowheight) {
+  for (let i = 0; i<10; i++) {
+  let myFun = random(F); 
+  let dy = random(y,y+rowheight/2); 
+  let x = random(0, width-50); 
+  myFun(x,dy); 
+  ypos = random(dy, ypos);  
   }
+}
   waldo(xpos,ypos); 
  }
 
