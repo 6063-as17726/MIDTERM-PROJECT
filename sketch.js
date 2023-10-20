f2e70034a9f97d4e9fe8401f447cc5b039e319d7
Ayushi Shah <as17726@nyu.edu>
2023-10-20 17:31:05 -0400

    fixed the overlap to a certain extent

--- sketch.js
+++ sketch.js
@@ -84,12 +84,11 @@ function waldo(X,Y) {
  // Pants
  fill(0, 0, 255);
  noStroke();
- rect(90, 150, 20, 20);
+ rect(0, 60, 20, 20);
 
  pop(); 
 }
 
-//need to update
 function character1(X,Y) {
   push();
   translate(X,Y);
@@ -98,17 +97,17 @@ function character1(X,Y) {
  strokeWeight(1); 
  stroke(0); 
  fill(255); 
- rect(115,30,5,140); 
+ rect(35,-40,5,140); 
 
  //stripes 
  strokeWeight(0); 
  fill(255,0,0); 
- rect(115,30,5,15); 
- rect(115,90,5,15); 
- rect(115,150,5,15); 
+ rect(35,-40,5,15); 
+ rect(35,20,5,15); 
+ rect(35,80,5,15); 
  fill(0,0,255); 
- rect(115,60,5,15); 
- rect(115,120,5,15); 
+ rect(35,-10,5,15); 
+ rect(35,50,5,15); 
 
 
  // Body
@@ -116,32 +115,31 @@ function character1(X,Y) {
  stroke(0);
  strokeWeight(1);
  beginShape();
- vertex (90, 100); 
- vertex(80,170); 
- vertex(120,170);
- vertex(110,100);
+ vertex (10,30); 
+ vertex(0,100); 
+ vertex(45,100);
+ vertex(30,30);
  endShape();  
 
  //beard 
  strokeWeight(1); 
  stroke(0); 
  fill(255); 
- triangle(90,90,110,90, 100,140);
+ triangle(10,30,30,30, 20,90);
  
  //face 
  strokeWeight(0); 
  fill("LightSalmon"); 
- ellipse(100, 85, 30,30); 
+ ellipse(20, 25, 30,30); 
 
  // Hat
  fill(0,0,255);
  noStroke();
- triangle(80, 75, 120, 75, 100, 40);
+ triangle(0, 15, 40, 15, 20, -50);
 
  pop(); 
 }
 
-//need to update
 function character2(X,Y) {
   push();
   translate(X,Y);
@@ -150,46 +148,52 @@ function character2(X,Y) {
  fill(255, 0, 0);
  stroke(0);
  strokeWeight(2);
- rect(90, 100, 20, 25);
- rect(90, 145, 20, 25);
+ rect(0, 10, 20, 25);
+ rect(0, 55, 20, 25);
 
   //hair 
   strokeWeight(0); 
   fill(108,80,26);
-  triangle(80, 100, 120, 100, 100, 50);
-
- //face 
- strokeWeight(0); 
- fill("LightSalmon"); 
- ellipse(100, 85, 30,30); 
-
- // Glasses
- fill(255);
- noStroke();
- ellipse(95,80,10,10);
- ellipse(105,80,10,10);
+  beginShape(); 
+  vertex(-10, 20); 
+  vertex(30, 20); 
+  vertex(20,-10); 
+  vertex(0, -10); 
+  endShape();
 
  // Striped Shirt
  fill(255, 255, 255);
  noStroke();
- rect(90, 100, 20, 10);
- rect(90, 115, 20, 10);
- rect(90, 150, 20, 10);
+ rect(0, 15, 20, 10);
+ rect(0, 30, 20, 10);
+ rect(0, 60, 20, 10);
+
+ //face 
+ strokeWeight(0); 
+ fill("LightSalmon"); 
+ ellipse(10, 5, 30,30); 
+
+  // Glasses
+  fill(255);
+  noStroke();
+  ellipse(5,0,10,10);
+  ellipse(15,0,10,10);
+ 
 
  // Hat
  fill(255, 0, 0);
  noStroke();
- triangle(80, 70, 120, 70, 100, 50);
+ triangle(-10,-5, 30,-5, 10, -20);
  strokeWeight(1); 
  stroke(255, 0, 0); 
  fill(255); 
- ellipse(100,45,10,10);
+ ellipse(10,-20,10,10);
 
 
  //Skirt
  fill(0, 0, 255);
  noStroke();
- rect(87, 125, 25, 20);
+ rect(-3, 35, 25, 20);
 
  pop();
 }
@@ -214,20 +218,23 @@ function setup() {
  }
 
 function draw() {
+
   let F = [character0,character1, character2]; 
   let rowheight = windowHeight/7;  
-  let xpos = random(0, width); 
+  let columnheight = windowWidth/25; 
+  let xpos = random(0, columnheight); 
   let ypos = random(0, rowheight);  
 
-for (let y = 0; y<height; y+=rowheight) {
-  for (let i = 0; i<10; i++) {
-  let myFun = random(F); 
-  let dy = random(y,y+rowheight/2); 
-  let x = random(0, width-50); 
-  myFun(x,dy); 
-  ypos = random(dy, ypos);  
+  for (let y = 0; y<height; y+=rowheight) {
+    for (let x = 0; x<width; x+=columnheight) {
+        let myFun = random(F); 
+        let dy = random(y,y+rowheight/2); 
+        let dx = random(x, x+columnheight); 
+        myFun(dx,dy); 
+        ypos = random(dy, ypos);  
+        xpos = random(dx, xpos); 
+    }
   }
-}
   waldo(xpos,ypos); 
  }
 
